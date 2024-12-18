fdd5d396e84ccadcf4bfdce4d30d5f1f711db30c
Silas Odero <Silas.Odero@cciea.corp>
2024-12-18 17:07:05 +0300

    added api that post the resources

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,6 +3,8 @@ using GameStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+const string GateGameEndPointName ="GateGame";
 List<GameDto> games =
 [
     new (
@@ -34,6 +36,26 @@ app.MapGet("games", () => games);
 
 //GET /games/1
 
-app.MapGet("games/{id}", (int id) =>games.Find(game =>  game.Id==id));
+app.MapGet("games/{id}", (int id) =>games.Find(game =>  game.Id==id))
+.WithName(GateGameEndPointName);
+
+// POST /games
+
+app.MapPost("games", (CreateGameDto newGame) =>
+{
+    GameDto game =new
+    (
+        games.Count +1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GateGameEndPointName, new{id =game.Id}, game);
+} 
+);
 
 app.Run();
