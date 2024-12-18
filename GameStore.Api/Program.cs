0ac029e84f3bb920bff3d414f1e14af52a2b4119
Silas Odero <Silas.Odero@cciea.corp>
2024-12-18 15:11:28 +0300

    Initial commit for GameStore Project

---
+++ GameStore.Api/Program.cs
@@ -0,0 +1,37 @@
+using GameStore.Api.Dtos;
+
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+List<GameDto> games =
+[
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992,7,15) 
+    ),
+
+    new(
+        2,
+        "Final Fantasy XIV",
+        "Roleplay", 
+        59.99M, 
+        new DateOnly(2010, 9,30)
+        ), 
+    new (
+        3,
+        "FIFA 23", 
+        "Sports", 
+        69.99M,
+        new DateOnly(2022,9,27) 
+    )  
+];
+
+app.MapGet("games", () => games);
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
